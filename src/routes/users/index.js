fea95973a92ad815dfd88054c4eae7e76d97e979
John-Kim <Leftium@users.noreply.github.com>
2022-06-16 00:14:03 +0900

    Log prisma.

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 export async function post({request}) {
   let data = await request.json();
   console.log({data});
+  console.log({prisma});
   const user = await prisma.user.create({data});
 
   return {
